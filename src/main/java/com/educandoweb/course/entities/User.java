581adf435be66377c97c00841dfb5eae09e5e783
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2023-12-28 09:45:40 -0300

    Orders
    Orders

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -36,6 +37,7 @@ public class User implements Serializable {
     /**
      * Associação com Pedidos, atributo onde um Cliente pode ter vários pedidos.
      */
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
